0e0714cf13b90cb2fb11edb68968fced3666f08e
Sergei <sm1.618@protonmail.com>
2023-09-25 21:46:49 -0600

    added some code

---
+++ app/StudentInfo.js
@@ -0,0 +1,15 @@
+
+
+function StudentInfo() {
+return (
+    <div>
+        <h1>Student Information</h1>
+        <p>Name: </p>
+        <p>Course Section: </p>
+        <p>GitHub Link: </p>
+    </div>
+    )
+}
+
+
+
