435e3af2e50fb8c9d1516194c156d1fe99cc9ee4
Sergei137 <sm1.618@protonmail.com>
2023-10-07 18:56:48 -0600

    removed styling from StudentInfo.js, functionality remains the same.

--- app/StudentInfo.js
+++ app/StudentInfo.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 export default function StudentInfo() {
 return (
-    <div className="border border-gray-300 p-4 rounded-lg shadow-md max-w-xs mx-auto">
+    <div>
         <p>Name: Sergei</p>
         <p>Course Section: CPRG 306 D</p>
         <p>GitHub Link: <Link href="https://github.com/Sergei137/cprg306-assignments" target="_blank">https://github.com/Sergei137/cprg306-assignments</Link></p>
