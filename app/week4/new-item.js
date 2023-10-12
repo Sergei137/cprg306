f2b5ce86e4042508b42cf648fb58350735897e8d
sergei137 <107164155+sergei137@users.noreply.github.com>
2023-10-12 15:32:39 -0600

    started week4 assignment

---
+++ app/week4/new-item.js
@@ -0,0 +1,23 @@
+"use strict";
+import { useState } from "react"
+
+function NameFunction() {
+    const [name, setName] = useState("");
+}
+
+function QuantityFunction() {
+    const [quantity, setQuantity] = useState(1);
+}
+
+function CategoryFunction() {
+    const [category, setCategory] = useState("produce");
+}
+
+
+
+
+
+
+
+
+
