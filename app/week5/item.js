c2f422fd62a9898b30ed73f6bc3ceda9b843600d
Sergei137 <sm1.618@protonmail.com>
2023-10-21 19:00:43 -0600

    update

--- app/week5/item.js
+++ app/week5/item.js
@@ -5,7 +5,7 @@ function Item({id, name, quantity, category}) {
     return (
         <div className="border border-black bg-white rounded-md p-4 mb-4">
             <ul>
-                <li>ID: {id}</li>
+                {/* <li>ID: {id}</li> */}
                 <li>Name: {name}</li>
                 <li>Quantity: {quantity}</li>
                 <li>Category: {category}</li>
