1d43fb0e968611b2c419b57c0dc1c8e3aac820a1
sergei137 <107164155+sergei137@users.noreply.github.com>
2023-10-21 19:00:07 -0600

    updates

--- app/week5/item-list.js
+++ app/week5/item-list.js
@@ -8,23 +8,60 @@ import items from './items.json'
 function ItemList() {
     const [sortBy, setSortBy] = useState('name');
 
+
+    // sort items by category, then by name
+    // const sortAll = (a, b) => {
+    //     if (a.category === b.category) {
+    //         return a.name.localeCompare(b.name);
+    //     } else {
+    //         return a.category.localeCompare(b.category);
+    //     }
+    // };
+
+
     // sort items by name or category
     const sortedItems = items.sort((a, b) => {
-        if (sortBy === 'name') {
-            return a.name.localeCompare(b.name);
-        } else if (sortBy === 'category') {
+        if (sortBy === 'name') { // sort by name
+            return a.name.localeCompare(b.name); 
+        } else if (sortBy === 'category') { // sort by category
             return a.category.localeCompare(b.category);
+        } else if (sortBy === 'sortAll') {
+            if (a.category === b.category) {
+                return a.name.localeCompare(b.name);
+            } else {
+                return a.category.localeCompare(b.category);
+            }
         } else {
             return 0;
         }
     });
 
+    // sortBy set to 'name'
+    const handleSortByName = () => {
+        setSortBy('name');
+    };
+
+    // sortBy set to 'category'
+    const handleSortByCategory = () => {
+        setSortBy('category');
+    };
+
+    // sortBy set to 'sortAll'
+    const handleSortAll = () => {
+        setSortBy('sortAll');
+    };
+
+
+
     return (
-        <div>
+        <div>   
+            <button onClick={handleSortByName}>Sort by Name</button>
+            <button onClick={handleSortByCategory}>Sort by Category</button>
+            <button onClick={() => setSortBy('sortAll')}>Sort All</button>
+            {/* <button onclick={handleSortAll}>Sort All</button> */}
             {sortedItems.map((item) => (
                 <Item id={item.id} name={item.name} quantity={item.quantity} category={item.category}/>
             ))}
-            
         </div>
     );
 };
