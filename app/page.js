0275595fc54283ce097937164875e4935622f367
Sergei137 <sm1.618@protonmail.com>
2023-11-13 17:11:36 -0700

    sync

--- app/page.js
+++ app/page.js
@@ -17,6 +17,7 @@ export default function RootPage() {
       <p><Link href="week5" target="_blank">Week 5</Link></p>
       <p><Link href="week6" target="_blank">Week 6</Link></p>
       <p><Link href="week7" target="_blank">Week 7</Link></p>
+      <p><Link href="week8" target="_blank">Week 8</Link></p>
     </div>
   );
 }
